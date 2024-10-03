f17cbae8dcd390fa9746282c8a79a02cecf2d6ca
unknown <joaofelixo674@gmail.com>
2024-10-03 18:14:27 +0000

    few changes

--- app.js
+++ app.js
@@ -1,35 +1,39 @@
-const express = require("express");
-const cors = require("cors");
-const helmet = require("helmet");
-const dotenv = require("dotenv");
-const app = express();
-const con = require("./utils/db");
+const express = require("express")
+const cors = require("cors")
+const helmet = require("helmet")
+const dotenv = require("dotenv")
+const app = express()
+const con = require("./utils/db")
 
 // configs
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(helmet());
-dotenv.config();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(helmet())
+dotenv.config()
 
 // main code
-con();
+con()
 
 // routes imports
 // const accountRoute = require("./routes/accountRoute");
-const activityRoute = require("./routes/activitiesRoute");
-const collectorRoute = require("./routes/collectorRoute");
-const paymentRoute = require("./routes/paymentRoute");
-const requestRoute = require("./routes/requestRoute");
-const userRoute = require("./routes/userRoute");
-const accountRoute = require("./routes/accountRoute");
+const activityRoute = require("./routes/activitiesRoute")
+const collectorRoute = require("./routes/collectorRoute")
+const paymentRoute = require("./routes/paymentRoute")
+const requestRoute = require("./routes/requestRoute")
+const userRoute = require("./routes/userRoute")
+const accountRoute = require("./routes/accountRoute")
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome to credit union api" })
+})
 
 // api
-app.use("/api/v1/users", userRoute);
-app.use("/api/v1/collectors", collectorRoute);
-app.use("/api/v1/activities", activityRoute);
-app.use("/api/v1/requests", requestRoute);
-app.use("/api/v1/payments", paymentRoute);
-app.use("/api/v1/accounts", accountRoute);
+app.use("/api/v1/users", userRoute)
+app.use("/api/v1/collectors", collectorRoute)
+app.use("/api/v1/activities", activityRoute)
+app.use("/api/v1/requests", requestRoute)
+app.use("/api/v1/payments", paymentRoute)
+app.use("/api/v1/accounts", accountRoute)
 
-port = process.env.port;
-app.listen(port, () => console.log(`listening on port ${port}`));
+port = process.env.port
+app.listen(port, () => console.log(`listening on port ${port}`))
